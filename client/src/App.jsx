5581446edf1dd772ae015396d1488179582cd1f1
aditya <adityaray947@gmail.com>
2024-01-22 14:33:14 +0530

    create pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,21 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Signin from './pages/Signin';
-import SignUp from './pages/SignUp';
-import About from './pages/About';
+import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import Signin from './pages/Signin'
+import Signup from './pages/SignUp'
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/sign-in' element={<Signin />} />
-        <Route path='/sign-up' element={<SignUp />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/search' element={<Search />} />
-      </Routes>
+        <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/about' element={<About/>}/>
+            <Route path='/profile' element={<Profile/>}/>
+            <Route path='/Signup' element={<Signup/>}/>
+            <Route path='/Signin' element={<Signin/>}/>
+        </Routes>
     </BrowserRouter>
-  );
+  )
 }
