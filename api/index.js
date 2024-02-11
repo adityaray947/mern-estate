f449be753b7880ad7972c14a6981bad28cc79469
aditya <adityaray947@gmail.com>
2024-02-11 12:40:17 +0530

    create lsiting API route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authrouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js'
 import cookieParser from "cookie-parser";
 dotenv.config();
 
@@ -23,6 +24,7 @@ app.listen(3000,()=>{
 
 app.use("/api/user",userRouter);
 app.use("/api/auth",authrouter);
+app.use("/api/listing",listingRouter);
 
 app.use((err,req,res,next)=>{
     const statusCode=err.statuscode || 500;
