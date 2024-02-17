a65f37f49aa33ee229c3509a72a7b18fc10e6c4f
aditya <adityaray947@gmail.com>
2024-02-18 02:40:34 +0530

    complete user listing func

--- client/src/pages/CreateLisitng.jsx
+++ client/src/pages/CreateLisitng.jsx
@@ -2,17 +2,18 @@ import React from 'react'
 import { useState } from 'react';
 import { getDownloadURL, getStorage, uploadBytesResumable, ref } from 'firebase/storage';
 import { app } from '../firebase';
+// import { set } from 'mongoose';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
 
 export default function CreateLisitng() {
     const {currentUser} =useSelector((state)=>state.user)
-    const [files, setFiles] = useState([]);
     const navigate = useNavigate();
+    const [files, setFiles] = useState([]);
 
     const [formData, setFormdata] = useState({
-        imageURLs: [],
+        imageUrls: [],
         name: '',
         description: '',
         address: '',
@@ -33,7 +34,7 @@ export default function CreateLisitng() {
    
     console.log(formData);
     const handleImageSubmit = (e) => {
-        if (files.length > 0 && files.length + formData.imageURLs.length < 7) {
+        if (files.length > 0 && files.length + formData.imageUrls.length < 7) {
             setUploading(true);
             setImageUploadError(false);
             const promises = [];
@@ -41,17 +42,19 @@ export default function CreateLisitng() {
             for (let i = 0; i < files.length; i++) {
                 promises.push(storeImage(files[i]));
             }
-            Promise.all(promises).then((urls) => {
-                setFormdata({
-                    ...formData,
-                    imageURLs: formData.imageURLs.concat(urls),
-                });
-                setImageUploadError(false);
-                setUploading(false);
-
-            }).catch((err) => {
-                setImageUploadError('Image upload failed(2 mb max per image)');
-            });
+            Promise.all(promises)
+        .then((urls) => {
+          setFormdata({
+            ...formData,
+            imageUrls: formData.imageUrls.concat(urls),
+          });
+          setImageUploadError(false);
+          setUploading(false);
+        })
+        .catch((err) => {
+          setImageUploadError('Image upload failed (2 mb max per image)');
+          setUploading(false);
+        });
 
         }
         else {
@@ -62,90 +65,96 @@ export default function CreateLisitng() {
 
     const storeImage = async (file) => {
         return new Promise((resolve, reject) => {
-            const storage = getStorage(app);
-            const fileName = new Date().getTime() + file.name;
-            const storageRef = ref(storage, fileName);
-            const uploadTask = uploadBytesResumable(storageRef, file)
-            uploadTask.on(
-                "state_changed",
-                (snapshot) => {
-                    const progress =
-                        (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-                    console.log(`upload is ${progress}%done`);
-                },
-                (error) => {
-                    reject(error);
-                },
-                () => {
-                    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-                        resolve(downloadURL);
-                    });
-                }
-            )
-
-        })
-    }
+          const storage = getStorage(app);
+          const fileName = new Date().getTime() + file.name;
+          const storageRef = ref(storage, fileName);
+          const uploadTask = uploadBytesResumable(storageRef, file);
+          uploadTask.on(
+            'state_changed',
+            (snapshot) => {
+              const progress =
+                (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+              console.log(`Upload is ${progress}% done`);
+            },
+            (error) => {
+              reject(error);
+            },
+            () => {
+              getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                resolve(downloadURL);
+              });
+            }
+          );
+        });
+      };
+    
     const handleRemoveImage = (index) => {
         setFormdata({
             ...formData,
-            imageURLs: formData.imageURLs.filter((_, i) => i !== index),
+            imageUrls: formData.imageUrls.filter((_, i) => i !== index),
         })
     }
     const handleChange = (e) => {
-
-        if (e.target.id === 'sell' || e.target.id === 'rent') {
-            setFormdata({
-              ...formData,
-              type: e.target.id,
-            })
+        if (e.target.id === 'sale' || e.target.id === 'rent') {
+          setFormdata({
+            ...formData,
+            type: e.target.id,
+          });
         }
-        if(e.target.id === 'parking' || e.target.id==='furnished' ||e.target.id === 'offer' ){
-            setFormdata({
-                ...formData,
-                [e.target.id]:e.target.checked,
-            });
-        }
+    
         if (
-            e.target.type === 'number' ||
-            e.target.type === 'text' ||
-            e.target.type === 'textarea'
-          ) {
-            setFormdata({
-              ...formData,
-              [e.target.id]: e.target.value,
-            });
-          }
-     };
-     const handleSubmit=async (e)=>{
-        e.preventDefault();
-        try{
-            if(formData.imageURLs.length<1) return setError('You must upload at least one image');
-            if(formData.regularPrice<+formData.discountPrice) return setError ('Discount Price must be lower than regular Price')
-            setLoading(true);
-            setError(false);
-            const res = await fetch('api/listing/create',{
-                method : 'POST',
-                headers:{
-                    'content-Type' : 'application/json',
-                },
-                body : JSON.stringify({
-                    ...formData,
-                    userRef: currentUser._id,
-                
-                }),
-
-            });
-            const data= await res.json();
-            setLoading(false);
-            if(data.success===false){
-                setError(data.message);
-            }
-            navigate(`/listing/${data._id}`)
-        }catch(error){
-            setError(error.message);
-            setLoading(false);
+          e.target.id === 'parking' ||
+          e.target.id === 'furnished' ||
+          e.target.id === 'offer'
+        ) {
+          setFormdata({
+            ...formData,
+            [e.target.id]: e.target.checked,
+          });
         }
-     }
+    
+        if (
+          e.target.type === 'number' ||
+          e.target.type === 'text' ||
+          e.target.type === 'textarea'
+        ) {
+          setFormdata({
+            ...formData,
+            [e.target.id]: e.target.value,
+          });
+        }
+      };
+    
+      const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+          if (formData.imageUrls.length < 1)
+            return setError('You must upload at least one image');
+          if (+formData.regularPrice < +formData.discountPrice)
+            return setError('Discount price must be lower than regular price');
+          setLoading(true);
+          setError(false);
+          const res = await fetch('/api/listing/create', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              ...formData,
+              userRef: currentUser._id,
+            }),
+          });
+          const data = await res.json();
+          setLoading(false);
+          if (data.success === false) {
+            setError(data.message);
+          }
+          navigate(`/listing/${data._id}`);
+        } catch (error) {
+          setError(error.message);
+          setLoading(false);
+        }
+      };
     return (
         <main className='p-3 max-w-4xl mx-auto'>
             <h1 className='text-3xl font-semibold text-center my-7'>
@@ -266,8 +275,8 @@ export default function CreateLisitng() {
                         </button>
                     </div>
                     <p className='text-red-700'>{imageUploadError && imageUploadError}</p>
-                    {formData.imageURLs.length > 0 &&
-                        formData.imageURLs.map((url, index) => (
+                    {formData.imageUrls.length > 0 &&
+                        formData.imageUrls.map((url, index) => (
                             <div
                                 key={url}
                                 className='flex justify-between p-3 border items-center'
