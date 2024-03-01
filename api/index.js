9c35085fc26aa842b9e139f10a461f031a0ad117
aditya <adityaray947@gmail.com>
2024-03-02 01:40:37 +0530

    commit2

--- api/index.js
+++ api/index.js
@@ -9,39 +9,43 @@ import path from 'path';
 dotenv.config();
 
 mongoose
-  .connect(process.env.mongo)
+  .connect(process.env.MONGO)
   .then(() => {
     console.log('Connected to MongoDB!');
-
-    const __dirname = path.resolve();
-    const app = express();
-    app.use(express.json());
-    app.use(cookieParser());
-
-    app.use('/api/user', userRouter);
-    app.use('/api/auth', authRouter);
-    app.use('/api/listing', listingRouter);
-
-    app.use(express.static(path.join(__dirname, '/client/dist')));
-
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-    })
-
-    app.use((err, req, res, next) => {
-      const statusCode = err.statusCode || 500;
-      const message = err.message || 'Internal Server Error';
-      return res.status(statusCode).json({
-        success: false,
-        statusCode,
-        message,
-      });
-    });
-
-    app.listen(3000, () => {
-      console.log('Server is running on port 3000!');
-    });
   })
   .catch((err) => {
     console.log(err);
   });
+
+  const __dirname = path.resolve();
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
+});
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
+
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
