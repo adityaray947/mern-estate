9513d7bf384bfa01f43a135e4527a272faff35de
aditya <adityaray947@gmail.com>
2024-03-01 20:22:52 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from './routes/user.route.js';
 import authrouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js'
 import cookieParser from "cookie-parser";
+import path from 'path';
 dotenv.config();
 
 mongoose.connect(process.env.mongo).then(()=>{
@@ -21,10 +22,16 @@ app.use(cookieParser());
 app.listen(3000,()=>{
     console.log("server is running");
 });
+const __dirname = path.resolve();
 
 app.use("/api/user",userRouter);
 app.use("/api/auth",authrouter);
 app.use("/api/listing",listingRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  })
 
 app.use((err,req,res,next)=>{
     const statusCode=err.statuscode || 500;
