2c19bf51c399ba2e9cd862a35d2265f89225c52d
aditya <adityaray947@gmail.com>
2024-03-02 00:33:44 +0530

    deploy to render

--- api/index.js
+++ api/index.js
@@ -18,13 +18,9 @@ mongoose
   });
 
   const __dirname = path.resolve();
-
 const app = express();
-
 app.use(express.json());
-
 app.use(cookieParser());
-
 app.listen(3000, () => {
   console.log('Server is running on port 3000!');
 });
